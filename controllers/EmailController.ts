5c7dd2a399455da2db31189f396b10532b213b0e
Vicky-shah <waqas@iintellect.co.uk>
2022-07-07 18:49:08 +0500

    changed file functio

--- controllers/EmailController.ts
+++ controllers/EmailController.ts
@@ -1,12 +1,10 @@
 import { Request, Response } from "express";
-import { Lo } from "../models/Lo";
 
 import {
   SEND_GRID_ACTIVITY_APPROVAL_TEMPLATE_ID,
   SEND_GRID_AFFILIATED_BLOG_DYNAMIC_TEMPLATE_ID,
   SEND_GRID_APPROVED_BLOG_POST_TEMPLATE_ID,
   SEND_GRID_EPOCH_BLOG_DYNAMIC_TEMPLATE_ID,
-  SEND_GRID_POST_ACTIVITY_TO_SOCIAL_TEMPLATE_ID,
 } from "../utils/constants";
 
 const sgMail = require("@sendgrid/mail");
@@ -141,72 +139,4 @@ export const sendEmailForActivityApprovalToAuthor = (
         return reject(error);
       });
   });
-};
-
-
-export const emailOnNewActivity = (
-  affiliateType : string,
-  websiteUrl : string,
-  postID : string,
-  activityType : string,
-  activityName : string,
-  activityDesc : string,
-  authorName : string,
-  imgSrc : string
-)=> {
-  return new Promise<any>((resolve, reject) => {
-
-  if (affiliateType === "LO") {
-    const loEmail = Lo.select(["lo_email"])
-      .where("website_url", "=", websiteUrl)
-      .get();
-    if (loEmail !== null) {
-      let templateId = SEND_GRID_POST_ACTIVITY_TO_SOCIAL_TEMPLATE_ID;
-
-      // prepare msg
-      const msg = {
-        to: 'waqas@blairallenagency.com',
-        from: {
-          email: "waqasshahh13@gmail.com",
-          name: "Affiliated Mortgage",
-        },
-        templateId,
-        dynamic_template_data: {
-          activityType,
-          activityName,
-          activityDesc,
-          authorEmail: authorName,
-          approveLink: `${websiteUrl}/wp-json/connectexpress/v1/shareonsocials?post_id=${postID}`,
-          imgSrc,
-        },
-      };
-      sgMail
-        .send(msg)
-        .then(() => {
-          console.log("Email sent");
-          return resolve({
-            success: true,
-            message: "Email sent",
-          });
-        })
-        .catch((err: any) => {
-          console.log(err.errors);
-          console.log(err);
-          return resolve({
-            success: false,
-            message: "Something went wrong please try again later",
-          });
-        });
-    }
-
-    return resolve({
-      success: false,
-      message: "No Lo with the given url found on our records",
-    });
-  }
-  return resolve({
-    success: false,
-    message: "Invalid affiliate Type",
-  });
-});
 };
